f4a7934292203c0f2365b6cb42ee89914ce2a271
Alexander Niedrist <alexander.niedrist@jambit.com>
2021-12-07 10:18:20 +0100

    improve day 1 and add gitignore

--- src/main/kotlin/day01/Solution.kt
+++ src/main/kotlin/day01/Solution.kt
@@ -3,34 +3,15 @@ package day01
 import util.FileReader
 
 fun main() {
-    val depths = FileReader.readResource("day01.txt").split("\n").map { it.toInt() }.toMutableList()
-    part1(depths)
-    part2(depths)
+    val d = FileReader.readResource("day01.txt").split("\n").map { it.toInt() }
+    part1(d)
+    part2(d)
 }
 
-fun part1(depths: MutableList<Int>) {
-    var counter = 0
-    var prev = depths.removeFirst()
-    depths.forEach {
-        if (prev < it)
-            counter++
-        prev = it
-    }
-    println(counter)
+fun part1(d: List<Int>) {
+    println( d.zipWithNext().count { (x, y) -> x < y } )
 }
 
-fun part2(depths: MutableList<Int>) {
-    var counter = 0
-    var prev2 = depths[1]
-    var prev1 = depths[2]
-    var prevTotal = prev2 + prev1 + depths[0]
-    depths.takeLast(depths.size - 3).forEach {
-        val current = prev2 + prev1 + it
-        if (prevTotal < current)
-            counter++
-        prevTotal = current
-        prev2 = prev1
-        prev1 = it
-    }
-    println(counter)
+fun part2(d: List<Int>) {
+    println(d.windowed(3, 1).zipWithNext().count{ (x, y) -> x.sum() < y.sum()})
 }
