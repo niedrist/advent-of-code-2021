f3af261918901d6030906818329ac0f5d6ccb895
Alexander Niedrist <alexander.niedrist@jambit.com>
2021-12-08 17:32:23 +0100

    add solution for day 3

--- src/main/kotlin/day03/Solution.kt
+++ src/main/kotlin/day03/Solution.kt
@@ -4,11 +4,12 @@ import util.FileReader
 import kotlin.math.pow
 
 fun main() {
-    val powerConsumption = FileReader.readResource("day03.txt").split("\n")
-    part1(powerConsumption)
+    val powerConsumption = FileReader.readResource("day03.txt").split("\n").toMutableList()
+    println(part1(powerConsumption))
+    println(part2(powerConsumption))
 }
 
-fun part1(powerConsumption: List<String>) {
+fun part1(powerConsumption: List<String>): Int {
     val counterMap = mutableMapOf<Int, Int>()
     powerConsumption.forEach {
         it.forEachIndexed { index, bit ->
@@ -18,11 +19,43 @@ fun part1(powerConsumption: List<String>) {
     }
     val maxBinaryIndex = powerConsumption[0].length - 1
     var gamma = 0
-    for (i in 0..maxBinaryIndex) {
-        val bit = (counterMap[maxBinaryIndex - i]!!.div((powerConsumption.size / 2)))
+    for (i in 0..maxBinaryIndex)
+        gamma += (counterMap.getOrDefault(maxBinaryIndex - i, 0).div((powerConsumption.size / 2))) * 2.0.pow(i).toInt()
 
-        gamma += bit * 2.0.pow(i).toInt()
+    return gamma * gamma.xor(2.0.pow(maxBinaryIndex + 1).toInt() - 1)
+}
 
+fun part2(powerConsumption: MutableList<String>): Int {
+    var l1 = powerConsumption.toMutableList()
+    var l2 = powerConsumption.toMutableList()
+    var i = 0
+    while(l1.size > 1) {
+        val mostCommon = bitCriteria(l1, i, true)
+        l1 = l1.filter { it[i] == mostCommon }.toMutableList()
+        i++
     }
-    println( gamma * gamma.xor(4095))
+    i = 0
+    while(l2.size > 1) {
+        val leastCommon = bitCriteria(l2, i, false)
+        l2 = l2.filter { it[i] == leastCommon }.toMutableList()
+        i++
+    }
+    return  binaryStringToInt(l1.first()) * binaryStringToInt(l2.first())
+}
+
+fun binaryStringToInt(s: String): Int {
+    var num = 0
+    for (i in s.indices)
+        num += (s[i].code - 48) * 2.0.pow(s.length - 1 - i).toInt()
+    return num
+}
+
+
+
+fun bitCriteria(bitsList: List<String>, index: Int, mostCommon: Boolean): Char {
+    val bits0 = bitsList.count { it[index] == '0'}
+    val bits1 = bitsList.count { it[index] == '1'}
+    if (mostCommon)
+        return if (bits0 > bits1) '0' else '1'
+    return if (bits0 <= bits1) '0' else '1'
 }
